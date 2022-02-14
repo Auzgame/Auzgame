cdf6d6dea50a914c8e13d5c546726db9cb82ddf4
Auzgame <93630725+Auzgame@users.noreply.github.com>
2022-02-14 10:47:13 -0500

    Update main.js

--- shellshocker/main.js
+++ shellshocker/main.js
@@ -1,1 +1,8 @@
-
+window.addEventListener('load',(e) => {
+  frame = document.createElement("iframe");
+  frame.src = "https://Web-Proxy.auzziescibelli.repl.co";
+  frame.width = 800;
+  frame.height = 600;
+  document.getElementById("center").appendChild(frame);
+  frame.contentWindow.postMessage("https://shellshock.io", "*");
+});
